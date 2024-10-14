fa42337b613166795e40cf46a2c7f5765d4e452d
karthik261201 <karthiknarayanan043@gmail.com>
2024-10-14 14:36:48 +0530

    implemented higher order components, debouncing, caching and nested comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,42 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { toggleMenu } from "../utils/appSlice"
+import { useEffect, useState } from "react"
+import { YOUTUBE_SEARCH_API } from "../utils/constants"
+import { cacheResults } from "../utils/searchSlice"
 
 const Header = () => {
 
+    const [searchQuery,setSearchQuery] = useState("")
+    const [suggestions,setSuggestions] = useState([])
+    const [showSuggestion,setShowSuggestion] = useState(false)
+    const searchCache = useSelector(store => store.search)
     const dispatch = useDispatch()
 
     const toggleMenuHandler = () => {
         dispatch(toggleMenu())
     }
+
+    const getSearchSuggestions = async () => {
+        const data = await fetch(YOUTUBE_SEARCH_API+searchQuery)
+        const json = await data.json()
+        setSuggestions(json[1])
+        dispatch(cacheResults({
+            [searchQuery]: json[1]
+        }))
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if(searchCache[searchQuery]) {
+                setSuggestions(searchCache[searchQuery])
+            }
+            else {
+                getSearchSuggestions()
+            }
+        }, 200)
+        return () => { clearTimeout(timer) }
+        // console.log(searchQuery)
+    },[searchQuery])
 
     return (
         <div className="grid grid-flow-col p-5 m-2 shadow-lg">
@@ -25,8 +54,27 @@ const Header = () => {
                 />
             </div>
             <div className="col-span-10">
-                <input className="w-1/2 border border-gray-500 p-1 rounded-l-full" type="text" />
-                <button className="border border-gray-500 px-3 py-1 rounded-r-full bg-gray-200">🔍</button>
+                <div>
+                    <input 
+                        className="px-5 w-1/2 border border-gray-500 p-1 rounded-l-full" 
+                        type="text" 
+                        value={searchQuery} 
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onFocus={() => setShowSuggestion(true)}
+                        onBlur={() => setShowSuggestion(false)}
+                    />
+                    <button className="border border-gray-500 px-3 py-1 rounded-r-full bg-gray-200">🔍</button>
+                </div>
+                {
+                    showSuggestion &&
+                    <div className="fixed bg-white py-2 px-5 w-[35rem] shadow-lg rounded-lg border border-gray-100">
+                        <ul>
+                            {
+                                suggestions.map(s => <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">🔍 {s}</li>)
+                            }
+                        </ul>
+                    </div>
+                }
             </div>
             <div className="col-span-1">
                 <img 
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { closeMenu } from "../utils/appSlice"
 import { useSearchParams } from "react-router-dom"
+import CommentsContainer from "./CommentsContainer"
 
 const WatchPage = () => {
 
@@ -14,15 +15,18 @@ const WatchPage = () => {
     },[])
 
     return (
-        <div className="px-5">
-            <iframe 
-                width="966" 
-                height="543" 
-                src={"https://www.youtube.com/embed/"+videoId+"?&autoplay=1"} 
-                title="YouTube video player" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                referrerpolicy="strict-origin-when-cross-origin">
-            </iframe>
+        <div className="flex flex-col">
+            <div className="px-5">
+                <iframe 
+                    width="966" 
+                    height="543" 
+                    src={"https://www.youtube.com/embed/"+videoId} 
+                    title="YouTube video player" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin">
+                </iframe>
+            </div>
+            <CommentsContainer />
         </div>
     )
 }
